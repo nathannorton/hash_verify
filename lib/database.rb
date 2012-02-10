df3b25125cbc9479994a664bbe71f8499b756c3f
Nathan Norton <nathan@nanoservices.com.au>
2012-02-10 16:48:04 +1100

    some more major refactor, split out some functionality into different modules and moved config files into conf

---
+++ lib/database.rb
@@ -0,0 +1,67 @@
+
+require 'socket'
+require 'bson'
+require 'mongo' 
+
+
+module Database
+  
+  class << self ; attr_accessor :server, :port, :database ; end
+  class << self ; attr_accessor :database, :collection ; end
+  class << self ; attr_accessor :username, :password ; end
+
+  @hostname = Socket.gethostname.split(".")[0]
+
+  # set up the mongo DB connection, use defaults if 
+  # the connection details in the config file are not set
+  def self.setup_db( opts, auth )
+    @port       = ( true && opts['port'] )       || 27017
+    @server     = ( true && opts['server'] )     || 'localhost'
+    @database   = ( true && opts['database'] )   || 'hash_verify'
+    @collection = ( true && opts['collection'] ) || 'hashes'
+
+    @username   = ( true && auth['username'] ) || 'hash_verify'
+    @password   = ( true && auth['password'] ) || 'hash_password'
+
+    db = Mongo::Connection.new( @server, @port ).db( @database )
+    raise "Failed to authenticate against the db" \
+      unless db.authenticate(username,password)
+
+    @collection = db.collection( collection )
+    db.strict = true
+  end
+
+  def self.publish_hash( hash, filename)
+    doc = { "path" => filename,
+            "host" => @hostname,
+            "date" => Time.now,
+            "algo" => hash.class.to_s,
+            "hash" => hash.to_s}
+
+    # check that the file is not already in the database
+    unless contains?( filename ) 
+      @collection.insert( doc )
+      puts "The following file has been added to the hash collection: #{filename}"
+    end
+  end
+
+  def self.publish_hashes( files ) 
+    files.each do |k,v|
+      publish_hash( v, k ) 
+    end
+  end
+
+  def self.search_hashes(  path ) 
+    @collection.find( "host" => @hostname , "path" => path).each {|row| return row}
+    return nil 
+  end
+
+
+  def self.contains?(  path ) 
+    @collection.find( "host" => @hostname , "path" => path).each {|row| return true}
+    return false
+  end
+
+
+
+end
---
+++ lib/hasher.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+# 
+# 
+
+# TODO: catch file exceptions 
+# TODO: make sure @algo is valid for sha and md5 only!
+
+require 'digest/md5'
+
+module Hasher 
+  # buffer for maximum size to chunk the hashing, in order to not 
+  # kill my  machine by loading a many gig file into memory
+  $BUF = 1024 * 1024 * 1024
+  
+  class << self ; attr_accessor :algo ; end
+
+  @algo = Digest::MD5
+
+  def self.generate_hashes( file_list ) 
+    list = {}
+    file_list.each do |path|
+      hash = generate_hash( path )  
+      list[path] = hash
+    end
+    list
+  end
+
+  def self.generate_hash( file ) 
+    return generate_hash_md5( file )  if @algo == Digest::MD5 
+    return generate_hash_sha2( file ) if @algo == Digest::SHA2    
+  end
+
+  def self.generate_hash_md5( file )
+    file_h = Digest::MD5.new
+    File.open(file, 'rb') do |fh|
+      while buffer = fh.read($BUF)
+        file_h.update( buffer )
+      end
+    end
+    file_h
+  end
+
+  def self.generate_hash_sha2( file )
+    file_h = Digest::SHA2.new
+    File.open(file, 'rb') do |fh|
+      while buffer = fh.read($BUF)
+        file_h.update( buffer )
+      end
+    end
+    file_h
+  end
+
+end
+
+
+
